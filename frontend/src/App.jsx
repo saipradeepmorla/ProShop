d38667ef64b3ab374982debbb299b12fa19f827f
SAI PRADEEP <65400733+saipradeep762@users.noreply.github.com>
2024-08-16 11:40:03 +0530

    frontend basic pages completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 
+import Header from './components/Header'
+import { Container } from 'react-bootstrap'
+import { Outlet } from 'react-router-dom'
+import Footer from './components/Footer'
+
 const App = () => {
   return (
-    <div>
-      Welcome to ProShop
-    </div>
+   <>
+   <Header />
+   <main className='py-3'>
+    <Container>
+     <Outlet />
+    </Container>
+   </main>
+   <Footer />
+   </>
   )
 }
 
